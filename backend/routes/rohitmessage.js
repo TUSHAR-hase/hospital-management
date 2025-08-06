7c8649cf64ef26cc2ce9b1c8522ffcd3fb85b219
TUSHAR-hase <thakortushar81315@gmail.com>
2025-08-06 14:45:36 +0530

    fix: removed Vite from Next.js project

--- backend/routes/rohitmessage.js
+++ backend/routes/rohitmessage.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { sendmail } from '../api/messagerohit';
+import { sendmail } from '../api/messagerohit.js';
 
 // import { sendmail } from '../api/message.js';
 
